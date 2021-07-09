429e04ec377ee064c75d91b3e7f8da4b41d6f652
Akashraj P Bhat <63663207+TechAkash-engg@users.noreply.github.com>
2021-07-09 17:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def login():
         email = request.form.get('emailid')
         password_input = request.form.get('password')
         details=validate_user(email)
-        if details[1]=='Student' and check_password_hash(details[0], password_input):
-            return redirect(url_for('stud_home', mail=email))
-        elif details[1]=='College' and check_password_hash(details[0], password_input):
-            return redirect(url_for('coll_home', mail=email))
+        if details[1] == 'Student' and check_password_hash(details[0], password_input):
+            return redirect(url_for('stud_home'))
+        elif details[1] == 'College' and check_password_hash(details[0], password_input):
+            return redirect(url_for('coll_home'))
         else:
             flash('Login Unsuccessful. Please check email and password')
     return render_template('login.html')
@@ -31,13 +31,12 @@ def login():
 def signup():
     if request.method == 'POST':
         choice = request.form.get('radiob')
-        print(choice)
         username=request.form.get('Username')
         email = str(request.form.get('emailid'))
         passw = request.form.get('password')
         confirm = request.form.get('cpassword')
         if passw == confirm:
-            register_user(email, username, passw,choice)
+            register_user(email, username, passw, choice)
             return redirect(url_for('login'))
         else:
             flash("Your credentials does not match,Try again")
@@ -45,12 +44,21 @@ def signup():
 
 
 @app.route('/studenthome', methods=['GET', 'POST'])
-def stud_home(mail):
+def stud_home():
     return render_template('index.html')
 
+
 @app.route('/collegehome', methods=['GET', 'POST'])
-def coll_home(mail):
-    return render_template('index1.html')
+def coll_home():
+    return render_template('index.html')
+
+@app.route('/forgot', methods=['GET', 'POST'])
+def forgot():
+    return render_template('forgot.html')
+
+@app.route('/reset', methods=['GET', 'POST'])
+def forgot():
+    return render_template('resetpassword.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
